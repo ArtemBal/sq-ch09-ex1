5bf2b1f33692a62059c0500c735c58fc925b728d
Artem Balan <a.s.balan96@gmail.com>
2024-01-24 19:57:24 +0200

    write tests

--- src/test/java/com/example/sqch09ex1/MainTests.java
+++ src/test/java/com/example/sqch09ex1/MainTests.java
@@ -1,13 +1,50 @@
 package com.example.sqch09ex1;
 
+import com.example.sqch09ex1.controllers.LoginController;
+import com.example.sqch09ex1.model.LoginProcessor;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.ui.Model;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 class MainTests {
 
+	@Mock
+	private Model model;
+
+	@Mock
+	private LoginProcessor loginProcessor;
+
+	@InjectMocks
+	private LoginController loginController;
+
 	@Test
-	void contextLoads() {
+	public void loginPostLoginSucceedsTest() {
+		given(loginProcessor.login()).willReturn(true);
+
+		String result = loginController.loginPost("username", "password", model);
+
+		assertEquals("login.html", result);
+
+		verify(model).addAttribute("message", "You are now logged in.");
+	}
+
+	@Test
+	public void loginPostLoginFailsTest() {
+		given(loginProcessor.login()).willReturn(false);
+
+		String result = loginController.loginPost("username", "password", model);
+
+		assertEquals("login.html", result);
+
+		verify(model).addAttribute("message", "Login failed!");
 	}
 
 }
