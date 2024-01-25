d46782a0255bbde3b3583bc00c0ed2c737a7c630
Artem Balan <a.s.balan96@gmail.com>
2024-01-25 20:05:58 +0200

    improve main page and controller

--- src/main/java/com/example/sqch09ex1/controllers/MainController.java
+++ src/main/java/com/example/sqch09ex1/controllers/MainController.java
@@ -2,7 +2,9 @@ package com.example.sqch09ex1.controllers;
 
 import com.example.sqch09ex1.services.LoggedUserManagementService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -14,10 +16,18 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String home() {
+    public String home(
+            @RequestParam(required = false) String Logout,
+            Model model
+    ) {
+        if (Logout != null) {
+            loggedUserManagementService.setUsername(null);
+        }
         String username = loggedUserManagementService.getUsername();
 
         if (username == null) return "redirect:/";
+
+        model.addAttribute("username", username);
         return "main.html";
     }
 }
